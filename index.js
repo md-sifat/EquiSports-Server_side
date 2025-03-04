ce4f83e67ecee8ff40adc410c70847c29d8e30f2
Md Sifat <mdsifat7252@gmail.com>
2025-03-04 20:02:38 +0600

    delete routes created for equipments

--- index.js
+++ index.js
@@ -58,8 +58,12 @@ async function run() {
             res.send(result);
         });
 
-
-
+        app.delete('/equipments/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await equipmentCollection.deleteOne(query);
+            res.send(result);
+        });
 
 
 
